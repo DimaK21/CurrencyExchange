ac32d40e5f22eaebdba414b4894fe1a7c26e6842
Dmitry Kryushnikov <civ02@mail.ru>
2025-02-19 23:52:52 +0500

    зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -45,4 +46,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.squareup.retrofit)
+    implementation(libs.retrofit.converter)
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
 }
