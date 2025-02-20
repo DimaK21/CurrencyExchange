2f9af0d72ef95207a6733ffdb419485794c72d0e
Dmitry Kryushnikov <civ02@mail.ru>
2025-02-21 03:31:09 +0500

    ExchangeRateResponse

--- app/src/main/java/ru/kryu/currencyexchange/data/network/ExchangeRateResponse.kt
+++ app/src/main/java/ru/kryu/currencyexchange/data/network/ExchangeRateResponse.kt
@@ -1,3 +1,21 @@
 package ru.kryu.currencyexchange.data.network
 
-data class ExchangeRateResponse(val rates: Map<String, Double>)
+import com.google.gson.annotations.SerializedName
+
+data class ExchangeRateResponse(
+    @SerializedName("base")
+    val base: String,
+    @SerializedName("rates")
+    val rates: Rates
+) {
+    data class Rates(
+        @SerializedName("GBP")
+        val gbp: Double?,
+        @SerializedName("USD")
+        val usd: Double?,
+        @SerializedName("EUR")
+        val eur: Double?,
+    )
+}
+
+
