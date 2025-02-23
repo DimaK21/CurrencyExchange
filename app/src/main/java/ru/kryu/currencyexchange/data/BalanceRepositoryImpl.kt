d3e1e51aa17f18c6283b202d6a2c623e396fae30
Dmitry Kryushnikov <civ02@mail.ru>
2025-02-23 21:59:49 +0500

    создание счета при первом обращении

--- app/src/main/java/ru/kryu/currencyexchange/data/BalanceRepositoryImpl.kt
+++ app/src/main/java/ru/kryu/currencyexchange/data/BalanceRepositoryImpl.kt
@@ -6,6 +6,7 @@ import androidx.datastore.preferences.core.doublePreferencesKey
 import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import ru.kryu.currencyexchange.domain.BalanceRepository
 import ru.kryu.currencyexchange.domain.model.Currency
 import javax.inject.Inject
@@ -15,9 +16,22 @@ class BalanceRepositoryImpl @Inject constructor(
 ) : BalanceRepository {
 
     override fun getBalances(): Flow<Map<Currency, Double>> {
-        return dataStore.data.map { preferences ->
+        return dataStore.data
+            .onStart { initBalance() }
+            .map { preferences ->
             Currency.entries.associateWith { currency ->
                 preferences[currency.name.toPreferencesKey()] ?: 100.0
+            }
+        }
+    }
+
+    private suspend fun initBalance() {
+        dataStore.edit { preferences ->
+            Currency.entries.forEach { currency ->
+                val key = currency.name.toPreferencesKey()
+                if (preferences[key] == null) {
+                    preferences[key] = 100.0
+                }
             }
         }
     }
